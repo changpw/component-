e217457d1523b9cd94714b856f2ed8a431195b65
常晓 <1589273884@qq.com>
2020-02-03 17:45:14 +0800

    输入框组件

---
+++ component-/component.js
@@ -0,0 +1,15 @@
+
+Vue.component('input-text',{
+	props:{
+		placeHolderValue:{
+			type:String,
+			default:'please input a value'
+		}
+	},
+	template:`<input class='inputClass' type="text" :placeholder="placeHolderValue" @input="sendValue($event)"></input>`,
+	methods:{
+		sendValue:function(e){
+			console.table(e.target.value)
+		}
+	}
+})
---
+++ demo.js
@@ -0,0 +1,15 @@
+		window.onload=function(){
+		const vm = new Vue({
+			el:"#app",
+			data:{
+				msg:'message',
+				placeHolder:"please input your name"
+			},
+			methods:{
+				
+			}
+		
+		})	
+	}
+	
+
